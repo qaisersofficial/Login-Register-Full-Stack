deecde315b31cc127d3535754cc7ba50b8e65ef6
QAISER ALI <qaisers.official@gmail.com>
2025-06-26 16:58:45 +0500

    added backend with register/login API using Express and MongoDB

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 
 dotenv.config();
-connectDB();
+
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+connectDB();
+
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
